8d1d255cf2329a907100dad2636b8c6cfbe19843
wiktoriasobczyk <wiktoria.sobczyk@htdevelopers.com>
2021-07-22 16:52:36 +0200

    replace htd starter to base flutter project

---
+++ android/app/src/main/kotlin/com/example/htd_power_skills/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.htd_power_skills
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- test_driver/app.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:flutter_starter_app/config.dart';
-import 'package:flutter_starter_app/start_app.dart';
-
-void main() {
-  enableFlutterDriverExtension();
-  return startApp(AppConfig.development());
-}
